6dc06a287b4a77143f0655838dc43ddc1ab9d886
xxlil_sy94 <xxlil_sy94@STHQ0124-08>
2025-06-27 19:05:16 +0400

    Add project files.

---
+++ Behavioral Design Pattern/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public interface IClient
+{
+    void DoSomething();
+}
+
+class Client
+{
+
+}
+
+class Adapter : IClient
+{
+    public void DoSomething()
+    {
+
+    }
+}
+
+class Service
+{
+    public void DoService()
+    {
+        Console.WriteLine("Do Service is worked");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
---
+++ Creational Design Pattern/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+interface IProduct
+{
+    string DoStuff();
+}
+
+class ConcreteProductA : IProduct
+{
+    public string DoStuff()
+    {
+        return "Product A worked";
+    }
+}
+
+class ConcreteProductB : IProduct
+{
+    public string DoStuff()
+    {
+        return "Product B worked";
+    }
+}
+
+abstract class Creator
+{
+    public abstract IProduct CreateProduct();
+
+    public void SomeOperation()
+    {
+        IProduct product = CreateProduct();
+        Console.WriteLine(product.DoStuff());
+    }
+}
+
+class ConcreteCreatorA : Creator
+{
+    public override IProduct CreateProduct()
+    {
+        return new ConcreteProductA();
+    }
+}
+
+class ConcreteCreatorB : Creator
+{
+    public override IProduct CreateProduct()
+    {
+        return new ConcreteProductB();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Creator creatorA = new ConcreteCreatorA();
+        creatorA.SomeOperation();
+
+        Creator creatorB = new ConcreteCreatorB();
+        creatorB.SomeOperation();
+    }
+}
