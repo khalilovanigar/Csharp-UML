a4ff8b0ec9aff9c7fe943d791d8a63a556e7f619
khalilovanigar <khalilovanigar64@gmail.com>
2025-06-29 13:56:34 +0400

    Update Program.cs

--- Structural Design Pattern/Program.cs
+++ Structural Design Pattern/Program.cs
@@ -1,10 +1,48 @@
-﻿using System;
+using System;
+
+public interface IClient
+{
+    void Method(string data);
+}
+
+public class Service
+{
+  public void ServiceMethod(string specialData)
+    {
+        Console.WriteLine($"Service is worked: {specialData}");
+    }
+}
+
+
+class Adapter : IClient
+{
+    
+    Service service = new Service(); 
+
+    public void Method(string data)
+    {
+        string specialData = "Adapted: " + data;
+        service.ServiceMethod(specialData);
+    }
+}
+
+public class Client
+{
+    public void DoSomething(IClient service)
+    {
+        service.Method("Client is doing something");
+    }
+}
+
 
 
 class Program
 {
     static void Main(string[] args)
     {
+        IClient adapter = new Adapter();
+        Client client = new Client();
+        client.DoSomething(adapter);
 
     }
 }
